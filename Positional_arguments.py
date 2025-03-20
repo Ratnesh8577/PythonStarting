7ab9316ffa0b3a53890994cd6685f83f45c1f29d
Ratnesh8577 <ratneshchauhan279@gmail.com>
2025-03-20 21:51:46 +0530

    Add files via upload

---
+++ Positional_arguments.py
@@ -0,0 +1,10 @@
+def total_marks(maths,science,english,hindi,history):
+    total = maths + science + english + hindi + history
+
+    print(total)
+total_marks(89,75,66,98,44)
+# ye sequence me hi data dena padega to isko overcome karne ke liye
+
+total_marks(english=66,history=44,science=75,maths=89,hindi=98)
+total_marks(english=66,history=44,hindi=98,science=75,maths=89)
+
