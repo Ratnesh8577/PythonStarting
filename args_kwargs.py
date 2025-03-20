85e7dff3378b67d1b972b7ec7c03302ebe7ac11a
Ratnesh8577 <ratneshchauhan279@gmail.com>
2025-03-20 21:52:42 +0530

    Add files via upload

---
+++ args_kwargs.py
@@ -0,0 +1,48 @@
+'''def add(*args):
+    print(args)# tuple aayega sabh
+add(1,2,3)
+add(200,3958,6,4,74,5)
+add(23,45)
+
+'''
+'''
+def add(*args):
+    print(sum(args))# sukar sakte hai
+add(1,2,3)
+add(200,3958,6,4,74,5)
+add(23,45)'''
+
+"""
+def add(*args,**kwargs): # Dictionary 
+    print(kwargs)# sukar sakte hai
+    for k,v in kwargs.items():
+        print(k,v)
+
+add(name="amit",age="23",gender="Male")"""
+
+
+
+def add(n1,n2,n3,*args,**kwargs): # Dictionary 
+    print(f"{n1=}")
+    print(f"{n2=}")
+    print(f"{n3=}")
+    print(f"{args=}")
+    print(f"{kwargs=}")
+    #print(kwargs)# sukar sakte hai
+    for k,v in kwargs.items():
+        print(k,v)
+
+add(5,10,15)
+
+
+def add(n1,n2,n3,*args,**kwargs): # Dictionary 
+    print(f"{n1=}")
+    print(f"{n2=}")
+    print(f"{n3=}")
+    print(f"{args=}")
+    print(f"{kwargs=}")
+    #print(kwargs)# sukar sakte hai
+    for k,v in kwargs.items():
+        print(k,v)
+
+add(5,10,15,100,200,300,name="Ratnesh")# String kwargs hota hai hamesa
